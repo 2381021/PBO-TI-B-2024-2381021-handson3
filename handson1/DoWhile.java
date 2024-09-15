2a1fd6791e8742e3397ada27120055200ddde9eb
Maharayatara <2381021@unai.edu>
2024-09-15 20:02:40 +0700

    melengkapi kodingan dan mengerjakan tugas kelas

--- handson1/DoWhile.java
+++ handson1/DoWhile.java
@@ -5,7 +5,7 @@ public class DoWhile {
         String[] kumpulanBuah = {"Apel", "Pisang", "Mangga", "Jeruk", "Durian", "Anggur" };
         int counter = 0;
         do {
-            System.out.println("Elemen ke " + (counter + 1) + "Adalah : " + kumpulanBuah[counter]);
+            System.out.println("Elemen ke " + (counter + 1) + " Adalah : " + kumpulanBuah[counter]);
             counter++;
         }while(counter < kumpulanBuah.length);
     }
---
+++ soalWhileLoop/WhileLoop.java
@@ -0,0 +1,13 @@
+package soalWhileLoop;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 20) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
